04b57dfc4e7c4c1dd709822ba20cba8ecbee9ec6
Ivan Abasov <ivan.abasov.t0126450@edu.atec.pt>
2025-02-25 15:10:58 +0000

    New_FeatureEx5

---
+++ Testes/Exer_5.cpp
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+int main(){
+
+    int num1;
+    int num2;
+    int num3;
+
+    printf("Escreve tres numeros: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+
+    printf("Crescente: ");
+   if (num1 <= num2 && num1 <= num3 ) {
+
+        printf("%d ", num1);
+        if (num2 <= num3) {
+            printf("%d %d ", num2, num3);
+        }else{
+
+            printf("%d %d ",num3, num2);
+
+        }
+   }else if (num2 <= num1 && num2 <= num3){
+
+        printf("%d ",num2);
+        if(num1 <= num3){
+            printf("%d %d ", num1, num3);
+        }else{
+
+            printf("%d %d ", num3, num1);
+
+        }
+
+   }else{
+
+        printf("%d ",num3);
+        if(num1 <= num2) {
+            printf("%d %d ", num1, num2);
+        }else{
+
+            printf("%d %d ",num2, num1);
+
+        }
+
+   }
+
+
+
+   printf("Decrescente: ");
+    for (int i = 0; i < 3; i++) {
+        if (i == 0) {
+            if (num1 >= num2 && num1 >= num3) {
+                printf("%d ", num1);
+                if (num2 >= num3) {
+                    printf("%d %d\n", num2, num3);
+                } else {
+                    printf("%d %d\n", num3, num2);
+                }
+            } else if (num2 >= num1 && num2 >= num3) {
+                printf("%d ", num2);
+                if (num1 >= num3) {
+                    printf("%d %d\n", num1, num3);
+                } else {
+                    printf("%d %d\n", num3, num1);
+                }
+            } else {
+                printf("%d ", num3);
+                if (num1 >= num2) {
+                    printf("%d %d\n", num1, num2);
+                } else {
+                    printf("%d %d\n", num2, num1);
+                }
+            }
+        }
+    }
+
+    return 0;
+}
