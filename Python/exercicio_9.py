4f44525547f90788de2f28707e5807e03ac343a8
IvanAbasov <ivanabasov27@gmail.com>
2025-04-23 10:44:47 +0100

    new_feature_ex9

---
+++ Python/exercicio_9.py
@@ -0,0 +1,10 @@
+while True:
+    num = int(input("Escreve um numero de entre 1 a 100: "))
+
+    if 1<= num <= 100:
+        break
+    else:
+        print("Não esta entre o numero de 1 a 100")
+
+print("Esse numero esta certo")
+    
