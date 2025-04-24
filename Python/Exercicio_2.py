c389331a24688ea11addcf73d462479688a80753
Ivan Abasov <ivan.abasov.t0126450@edu.atec.pt>
2025-04-24 10:45:23 +0100

    fature_ex2

--- Python/Exercicio_2.py
+++ Python/Exercicio_2.py
@@ -1,4 +1,5 @@
 num = []
+impar= []
 
 print("Escreve 10 numeros\n")
 
@@ -6,5 +7,8 @@ for i in range (10):
     numero = int(input("Escreve um numero: "))
     if numero % 2 == 0:
         num.append(numero)
+    else:
+        impar.append(numero)
 
 print(num)
+print(impar)
